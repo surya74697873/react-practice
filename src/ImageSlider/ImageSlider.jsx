5b2d41c8396fdc7ca6a22132f5ef1d98c8ecaeab
surya74697873 <surya74697873@gmail.com>
2024-11-04 16:20:01 +0530

    ImageSlider Works Done

--- src/ImageSlider/ImageSlider.jsx
+++ src/ImageSlider/ImageSlider.jsx
@@ -1,65 +1,102 @@
 import { useEffect, useState } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { BiArrowFromLeft, BiArrowFromRight } from "react-icons/bi";
-import './ImageSlider.css'
+import "./ImageSlider.css";
 
 const ImageSlider = ({ url, page = 5, limit = 0 }) => {
+  const [currentSlider, setCurrentSlider] = useState(0);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [currentSlider, setCurrentSlider] = useState(0);
-    const [images, setImages] = useState([]);
-    const [loading, setLoading] = useState(false)
-
-
-    async function apiCall(getURL) {
-        try {
-            setLoading(true)
-            let response = await fetch(`${getURL}?page=${page}&limit=${limit}`)
-            let data = await response.json()
-            setImages(data)
-            setLoading(false)
-            console.table(images);
-
-        } catch (error) {
-            console.log(error);
-            setLoading(false)
-        }
-
+  async function apiCall(getURL) {
+    try {
+      setLoading(true);
+      let response = await fetch(`${getURL}?page=${page}&limit=${limit}`);
+      let data = await response.json();
+      setImages(data);
+      setLoading(false);
+      console.table(images);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
+  }
 
-    useEffect(() => {
-        if (url != "")
-            apiCall(url)
-    }, [url, page, limit])
+  useEffect(() => {
+    if (url != "") apiCall(url);
+  }, [url, page, limit]);
 
-    function prevImage() {
-        setCurrentSlider((prev) => prev == 0 ? images.length - 1 : prev - 1)
-    }
+  function prevImage() {
+    setCurrentSlider((prev) => (prev == 0 ? images.length - 1 : prev - 1));
+  }
 
-    function nextImage() {
-        setCurrentSlider((next) => next == images.length - 1 ? 0 : next + 1)
-    }
+  function nextImage() {
+    setCurrentSlider((next) => (next == images.length - 1 ? 0 : next + 1));
+  }
 
-    if (loading)
-        return <div>Loading... Please Wait</div>
+  if (loading) return <div id="load">Loading... Please Wait</div>;
 
+  return (
+    <div id="container">
+      <BiArrowFromLeft
+        onClick={prevImage}
+        style={{
+          cursor: "pointer",
+          fontSize: "1.5em",
+          position: "absolute",
+          background: "white",
+          bottom: "50%",
+          borderRadius: "50%",
+          left: "0.5em",
+        }}
+      />
 
-    return (
-        <div id="container">
-            <BiArrowFromLeft onClick={prevImage} style={{ cursor: "pointer" }} />
+      {images.length > 0 ? (
+        images.map((image, index) => (
+          <img
+            src={image.download_url}
+            alt={image.download_url}
+            key={index}
+            className={currentSlider !== index ? "inactive" : "active"}
+          />
+        ))
+      ) : (
+        <p>Images Not Received</p>
+      )}
 
-            {images.length > 0 ? images.map((image, index) => 
-                 <img src={image.download_url} alt={image.download_url} key={index} />
-            ) : <p>Images Not Received</p>}
+      <BiArrowFromRight
+        onClick={nextImage}
+        style={{
+          cursor: "pointer",
+          fontSize: "1.5em",
+          position: "absolute",
+          background: "white",
+          bottom: "50%",
+          borderRadius: "50%",
+          right: "0.5em",
+        }}
+      />
 
-            <BiArrowFromRight onClick={nextImage} style={{ cursor: "pointer" }} />
-        </div>
-    );
-}
+      <div className="round-bar">
+        {[...Array(images.length)].map((_, index) => {
+          return (
+            <div
+              key={index}
+              className={
+                currentSlider !== index ? "inactive-round" : "active-round"
+              }
+            ></div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 ImageSlider.propTypes = {
-    url: PropTypes.string.isRequired,
-    page: PropTypes.number,
-    limit: PropTypes.number,
-}
+  url: PropTypes.string.isRequired,
+  page: PropTypes.number,
+  limit: PropTypes.number,
+};
 
 export default ImageSlider;
