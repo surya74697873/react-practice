71b6e1e5ccd7dfdbf0272801c12545b1c8cea36a
surya74697873 <surya74697873@gmail.com>
2024-11-12 09:11:52 +0530

    Quiz Component Over

---
+++ src/Quiz/Data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        "id" : 1,
+        "questions" : "What is the Capital of France ?",
+        "options" : ["Berlin", "Madrid", "Paris", "Rome"],
+        "correctOption" : "Paris"
+    },
+    {
+        "id" : 2,
+        "questions" : "Which Planet is known as the Red Planet ?",
+        "options" : ["Venus", "Mars", "Jupiter", "Saturn"],
+        "correctOption" : "Mars"
+    },
+    {
+        "id" : 3,
+        "questions" : "What kind of small objects compose much of the universe ?",
+        "options" : ["Atoms", "Dark Matter", "Anti Matter", "None"],
+        "correctOption" : "Dark Matter"
+    },
+    {
+        "id" : 4,
+        "questions" : "What is the Shape of the Earth ?",
+        "options" : ["Round", "Flat", "Has no shape", "SpHeroid"],
+        "correctOption" : "SpHeroid"
+    },
+    {
+        "id" : 5,
+        "questions" : "Which is a hypothetical string theory weightless particle ?",
+        "options" : ["Graviton", "Earth Worm Jim", "Anti-matter","Baryonic Matter"],
+        "correctOption" : "Graviton"
+    },
+    {
+        "id" : 6,
+        "questions" : "What is the chemical symbol for the element commonly known as 'table salt' ?",
+        "options" : ["Ti", "NaCl", "CO2","H2O"],
+        "correctOption" : "NaCl"
+    },
+    {
+        "id" : 7,
+        "questions" : "Which is the largest animal that is at present thriving ?",
+        "options" : ["Blue Whale", "Crocodile", "Elephant","tiger"],
+        "correctOption" : "Blue Whale"
+    },
+    {
+        "id" : 8,
+        "questions" : "An Array is what kind of data structure ?",
+        "options" : ["Linear", "Non Linear", "Complex","None"],
+        "correctOption" : "Linear"
+    },
+    {
+        "id" : 9,
+        "questions" : "A Stack follows the principle of ?",
+        "options" : ["LIFO", "FIFO", "Both","None"],
+        "correctOption" : "LIFO"
+    },
+    {
+        "id" : 10,
+        "questions" : "Ensure a class has only one instance, and provide a global access point to it ?",
+        "options" : ["Single Class Factory", "Proxy", "Singleton","Flyweight"],
+        "correctOption" : "Singleton"
+    },
+]
+
+export default data;
--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,22 +1,76 @@
-import './Quiz.css'
+import { useState } from "react";
+import data from "./Data";
+import "./Quiz.css";
+import { useEffect } from "react";
 
 const Quiz = () => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [scoreCard, setScoreCard] = useState(false);
+  const [timer, setTimer] = useState(10)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => {
+        if (prevTimer === 1) {
+          if (currentQuestion < data.length - 1) {
+            setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+            return 10; 
+          } else {
+            setScoreCard(true);
+            clearInterval(interval);
+            return prevTimer;
+          }
+        } else {
+          return prevTimer - 1;
+        }
+      });
+    },1000)
+
+    return () => clearInterval(interval)
+  },[currentQuestion,timer])
+
+  function checkCorrections(option) {
+    if (data[currentQuestion].correctOption === option) {
+      setScore((prevScore) => prevScore + 1);
+    }
+
+    if (currentQuestion < data.length - 1){
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+    }
+    else
+      setScoreCard(true)
+    setTimer(10)
+  }
+
+
+  function restartQuiz(){
+    setCurrentQuestion(0)
+    setScore(0)
+    setScoreCard(false)
+  }
+
   return (
     <div className="Quiz">
-      <div className="score-section" >
-        <h2>Your Score : 3/3</h2><button>Restart</button>
-      </div>
-      <div className="question-section">
-        <h2>Quesiton 1</h2>
-        <p>This is sample Question</p>
-        <class className="options">
-          <button>option1</button>
-          <button>option2</button>
-          <button>option3</button>
-          <button>option4</button>
-        </class>
-        <div className="timer">Time Left : 5s</div>
-      </div>
+      {scoreCard ? (
+        <div className="score-section">
+          <h2>Your Score : {`${score} / ${data.length} `}</h2>
+          <button onClick={restartQuiz}>Restart</button>
+        </div>
+      ) : (
+        <div className="question-section">
+          <h2>Question {currentQuestion + 1}</h2>
+          <p>{data[currentQuestion].questions}</p>
+          <div className="options">
+            {data[currentQuestion].options.map((option, index) => (
+              <button key={index} onClick={() => checkCorrections(option)}>
+                {option}
+              </button>
+            ))}
+          </div>
+          <div className="timer">Time Left : {timer}s</div>
+        </div>
+      )}
     </div>
   );
 };
